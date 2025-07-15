fa0786a81963f7889c046b6a8ca2b7bad6df2919
Camilo-Gomez <camilo.gomez114@gmail.com>
2025-07-15 14:37:25 -0500

    feat: update CORS configuration to allow multiple origins for the application

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173', // Permite solo tu frontend
-  });
+  app.enableCors( [
+    "http://localhost:5173",                       // Desarrollo local
+    "https://perfumeria-sistema.vercel.app",        // Dominio de Vercel
+    "https://perfumeria-sistema-juan-camilo-gomezs-projects-6585cabf.vercel.app", // Deploys temporales de Vercel
+  ]);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
